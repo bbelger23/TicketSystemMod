deec65a63f48ceb0e7e88c98b0a260c431dd87ec
Brandon Belger <bbelger@wctc.edu>
2022-03-08 20:44:17 -0600

    added ticketfile.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using NLog.Web;
+using System.IO;
+using System.Collections.Generic;
 
 namespace TicketSystemMod
 {
     class Program
     {
+        private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string defectFilePath = Directory.GetCurrentDirectory() + "\\tickets.csv";
+            string enhanceFilePath = Directory.GetCurrentDirectory() + "\\enhancements.csv";
+            string taskFilePath = Directory.GetCurrentDirectory() + "\\tasks.csv";
+
+            logger.Info("Program Started");
+
+            DefectFile defectFile = new DefectFile(defectFilePath);
+            EnhanceFile enhanceFile = new EnhanceFile(enhanceFilePath);
+            TaskFile taskFile = new TaskFile(taskFilePath);
+            
+             
         }
     }
 }
---
+++ TicketFile.cs
@@ -0,0 +1,35 @@
+using System;
+using NLog.Web;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TicketSystemMod
+{
+    //public string defectPath {get;set;}
+    //public string enhancePath {get;set;}
+    //public string taskPath {get;set;}
+    public class DefectFile
+    {
+        public DefectFile(string defectFilePath)
+        {
+            
+        }
+    }
+
+    public class EnhanceFile
+    {
+        public EnhanceFile (string enhanceFilePath)
+        {
+
+        }
+    }
+
+    public class TaskFile
+    {
+        public TaskFile (string taskFilePath)
+        {
+
+        }
+    }
+}
